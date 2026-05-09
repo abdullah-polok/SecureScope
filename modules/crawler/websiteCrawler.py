e67d1579227aae2d857ea785aa35ef9971fdf1fd
abdullah-polok <abdullahalrahman23@gmail.com>
2026-05-09 10:58:26 +0300

    Added Website Crawler module using Beautifulsoup and requests

---
+++ modules/crawler/websiteCrawler.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+# A set to keep track of visited URLs
+visited = set()
+
+# Get the URL to crawl from the user
+url = input("Enter the URL to crawl: ")
+
+# make a get request from the website using requests library
+response = requests.get(url)
+
+# parse html content using BeautifulSoup library
+
+soup = BeautifulSoup(response.content,"html.parser")
+
+# find all anchor tags and extract the href attribute
+#https://www.crummy.com/software/BeautifulSoup/bs4/doc/#calling-a-tag-is-like-calling-find-all
+for link in soup.find_all("a"):
+    # extract the href attribute from the anchor tag
+    href = link.get("href")
+
+
+# check if the href is not None and has not been visited before
+    if href and href not in visited:
+        # add the href to the visited set
+        visited.add(href)
+        print(href)
+
+
