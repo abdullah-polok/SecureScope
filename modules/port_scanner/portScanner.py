edfc12a845b1dd3f57e18ee4f4885a5bdec0acad
abdullah-polok <abdullahalrahman23@gmail.com>
2026-05-11 00:58:12 +0300

    Added Web login gusser using requests library

--- modules/port_scanner/portScanner.py
+++ modules/port_scanner/portScanner.py
@@ -1,3 +1,18 @@
+"""
+This module provides a simple port scanner that can be used to check for open ports on a target IP address. 
+The user can input the target IP address and a list of ports to scan, and the module will return a list of open ports.
+
+Development Steps
+=> Import necessary libraries (socket)
+=> Define a function to scan ports
+=> Create a socket and try to connect to the target on each specified port
+=> Set a timeout for the connection
+=> Check the result of each connection attempt and store open ports
+=> Close the socket after each try
+=> Get user input for target IP address and ports to scan
+
+"""
+
 import socket
 
 def scan_ports(target,ports):
@@ -13,6 +28,7 @@ def scan_ports(target,ports):
             open_ports.append(port)
         sock.close()
     return open_ports
+
 
 input_target=input("Enter the target IP address: ")
 input_ports=input("Enter the ports to scan (comma separated): ")
---
+++ modules/web_login_guesser/webLoginGusser.py
@@ -0,0 +1,37 @@
+"""
+This module is used to test login forms with multiple username and password combinations.
+Demonstrates risks of weak passwords and poor authentication security and
+Commonly used in penetration testing and security auditing
+
+Development Steps
+
+=> Identify target login page
+=> Analyze login request parameters
+=> Load username or password wordlist
+=> Send automated login attempts
+=> Detect successful or failed responses
+=> Store and display results
+"""
+
+import requests
+
+def web_login_gusser(url, username, password):
+# This function try to log in to a web application using the provided URL, username, and password  
+    try:
+        #send a post request to the login website with the given username and password
+        response = requests.post(url, data={'username': username, 'password': password})
+   
+      # check the reponse statud code and print the result of the login username and password 
+        if response.status_code == 200:
+            print(f"Login successful for {username}:{password}")
+        else:
+            print(f"Login failed for {username}:{password}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+input_url = input("Enter the login URL: ")
+input_username = input("Enter the username: ")
+input_password = input("Enter the password: ")
+
+web_login_gusser(input_url, input_username, input_password)
