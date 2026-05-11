d800dfa7cdc17e7de714e4661294bd294e57ed10
abdullah-polok <abdullahalrahman23@gmail.com>
2026-05-11 11:54:52 +0300

    Add:Pack sniffer module using python scapy library

---
+++ modules/packet_sniffer/packetSniffer.py
@@ -0,0 +1,29 @@
+"""
+This module implements a simple packet sniffer using the Scapy library. 
+It captures network packets on a specified interface and displays their contents.
+
+Development steps:
+=> Import necessary libraries (Scapy)
+=> Define a function to start sniffing on a given interface
+=> Define a callback function to process each sniffed packet
+=> Get the interface to sniff on from user input
+
+"""
+
+import scapy.all as scapy
+
+
+
+# Function to start sniffing on a specified interface
+def sniffer(interface):
+# Use Scapy's sniff function to capture packets on the specified interface    
+    scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
+
+# Callback function to process each sniffed packet
+def process_sniffed_packet(packet):
+   # Print the summary of the packet 
+    print(packet.show())
+
+
+input_interface = input("Enter the interface to sniff on: ")
+sniffer(input_interface)
